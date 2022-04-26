3fd39113ddf8acf48fa7364a0730ae9a7fabda67
ehqkr <ghyoo0915@naver.com>
2022-04-26 11:48:37 +0900

    유광현 - 통계 1148

--- src/main/java/com/naedam/mir9/statistics/model/dao/StatisticsDao.java
+++ src/main/java/com/naedam/mir9/statistics/model/dao/StatisticsDao.java
@@ -1,5 +1,12 @@
 package com.naedam.mir9.statistics.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import com.naedam.mir9.statistics.model.vo.PeriodStatisticVo;
+
 public interface StatisticsDao {
+
+	PeriodStatisticVo selectPeriodStatistics(Map<String, Object> param);
 
 }
